7c63f89819c9a4ed12092dbf3964ac9e4ab2506a
Dawit Berhane <dawit.berhane711@gmail.com>
2020-05-09 23:48:41 -0500

    data loading code

--- StudentGradesProject/src/studentRecord/Main.java
+++ StudentGradesProject/src/studentRecord/Main.java
@@ -1,9 +1,21 @@
 package studentRecord;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException{
+		List<StudentRecord> records = new ArrayList<StudentRecord>();
+		Path path = Paths.get("Data", "Student Grade Record.txt");
+		
+		records = DataLoader.parallelLoader(path);
+		
+		
+		
 
 	}
 
