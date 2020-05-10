553e01be40c8bc047a1e98a4fa8dfc8231b397ff
Dawit Berhane <dawit.berhane711@gmail.com>
2020-05-09 22:09:59 -0500

    Added students GPA statistics method

--- StudentGradesProject/src/studentRecord/ParallelStudentGradesStatistics.java
+++ StudentGradesProject/src/studentRecord/ParallelStudentGradesStatistics.java
@@ -1,5 +1,6 @@
 package studentRecord;
 
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -18,4 +19,21 @@ public class ParallelStudentGradesStatistics {
 
 	}
 	
+	public static void studentsGPAStatistics( List<StudentRecord> records ) {
+		System.out.println("=========================================");
+		System.out.println(" Student GPA statistics (highest and lowest GPA) ");
+		Map<String, Double> studentsGradeMap = records
+				.parallelStream()
+				.collect(Collectors.groupingBy(StudentRecord::getStudentID, 
+						Collectors.averagingDouble(StudentRecord::getStudentGrade)));
+		
+		List<StudentGPA> studentGPA = studentsGradeMap.entrySet().parallelStream()
+				.map(entry -> new StudentGPA(entry.getKey(), entry.getValue()))
+				.collect(Collectors.toList());
+		DoubleSummaryStatistics gpaStatistics = studentGPA
+				.parallelStream()
+				.collect(Collectors.summarizingDouble(StudentGPA::getGpa));
+		System.out.println("Highest GPA student: "+ gpaStatistics.getMax());
+		System.out.println("Lowest GPA student: "+ gpaStatistics.getMin());
+	}
 }
